fb6e87034251b31ac378456f63a2ac3aa83725e3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-24 22:05:52 -0300

    feat: implementa calculo de custo bruto, desconto e o valor final

--- Program.cs
+++ Program.cs
@@ -9,3 +9,14 @@ string cupomDesconto = Console.ReadLine() ?? string.Empty;
 
 Console.Write("Idade do Cliente: ");
 int idadeCliente = Convert.ToInt32(Console.ReadLine());
+
+const decimal taxaConveniencia = 5.00m;
+const decimal precoIngresso = 25.00m;
+
+decimal custoBruto = taxaConveniencia + (precoIngresso * quantidadeIngressos);
+
+decimal desconto = cupomDesconto.Trim().Equals("CINEMA10", StringComparison.OrdinalIgnoreCase)
+    ? custoBruto * 0.10m
+    : 0.00m;
+
+decimal valorFinal = custoBruto - desconto;
