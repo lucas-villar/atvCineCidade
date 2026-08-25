5d9509f0d4070f7bd7a45e3d95b42869a4b6580e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-24 22:03:53 -0300

    feat: criando estrutura inicial do app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+Console.Write("Nome do Cliente: ");
+string nomeCliente = Console.ReadLine() ?? string.Empty;
+
+Console.Write("Quantidade de Ingressos: ");
+int quantidadeIngressos = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Cupom de Desconto: ");
+string cupomDesconto = Console.ReadLine() ?? string.Empty;
+
+Console.Write("Idade do Cliente: ");
+int idadeCliente = Convert.ToInt32(Console.ReadLine());
